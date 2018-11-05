caaf4489082a987e3361c7ce4e7cb8252f5d316d
Clément Fang <clement.fang@epita.fr>
2018-11-05 02:03:17 +0100

    Fix some bugs when compute weight

--- em.py
+++ em.py
@@ -1,22 +1,9 @@
 import math
 import numpy as np
 import random
-import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
-
-colors = ["r", "g", "b", "peachpuff", "fuchsia"]
-size_color = len(colors)
-
-def plot_data(data):
-    for j in range (len(data)):
-        plt.plot(data[j], colors[j % size_color])
-
-    plt.axis([-100, 100, -100, 100])
-    plt.show()
-
 
 def sdp_mat(dim, sd):
-    M = np.matrix(np.random.rand(dim, dim))
+    M = np.array(np.random.rand(dim, dim))
     M = 0.5 * (M + M.T)
     M = M + sd * np.eye(dim)
     return M
@@ -30,20 +17,27 @@ def get_sdp_mat(sd_diff, nb_gauss, dim):
     return [sdp_mat(dim, i) for i in SDClass[0]]
 
 def gen_gmm(theta, nb_pts, nb_gauss, mu, cov):
-
     r = np.random.multinomial(nb_pts, theta)
 
-    data = [np.random.multivariate_normal(mu[i], cov[i], r[i]) for i in range(0, nb_gauss)]
+    data = []
+    for i in range (nb_gauss):
+        gmm = np.random.multivariate_normal(mu[i], cov[i], r[i])
+        for j in range (r[i]):
+            data.append(gmm[j])
+    """
+    data = np.random.multivariate_normal(mu[0], cov[0], nb_pts) #fixme  [nb_pts, dim]
+    """
     return data
 
-def update_mu(s_weight, data_array, weights_array):
-    mu = np.zeros([dim])
+def update_mu(mu, s_weight, data, weights_array):
+    for d in range (len(data[0])):
+        mu[d] = 0
 
-    for i in range (len(data_array)):
+    for i in range (len(data)):
         for d in range (dim):
-            mu[d] += data_array[i][d] * weights_array[i]
+            mu[d] += data[i][d] * weights_array[i]
 
-    return mu / s_weight
+    mu /= s_weight
 
 def var_flor(s, f):
     l = np.linalg.cholesky(s)
@@ -64,11 +58,7 @@ def update_cov(s_weight, data_array, mu_array, weights_array):
     cov = np.zeros([dim, dim])
 
     for i in range (len(data_array)):
-#        print ("update cov ")
-#        print (cov)
-
         mat = np.asmatrix(data_array[i]) - np.asmatrix(mu_array)
-
         cov += weights_array[i] * (mat.T @ mat)
 
     return cov / s_weight
@@ -78,92 +68,112 @@ def log_multivar(point, dim, mu, cov):
     e = -0.5 * (point - mu).T @ np.linalg.inv(cov) @ (point - mu)
     return q + e
 
-def compute_weight(nb_pts, nb_gauss, dim, alpha, data, mu, cov):
-    weights = np.zeros([nb_gauss, nb_pts])
-
+def compute_weight(weights, nb_gauss, dim, alpha, data, mu, cov):
     llk = 0
-
-    for j in range (nb_gauss):
+    for i in range (len(data)):
         maxi = -math.inf
-        for i in range (len(data[j])):
-            point = np.empty([dim])
-            for d in range (dim):
-                point[d] = data[j][i][d]
+        for j in range (nb_gauss):
+            weights[j][i] = log_multivar(data[i], dim, mu[j], cov[j])
+            weights[j][i] += np.log(alpha[j])
 
-            weights[j][i] = log_multivar(point, dim, mu[j], cov[j]) + np.log(alpha[j])
-
-            if maxi < weights[j][i]:
+            if weights[j][i] > maxi:
                 maxi = weights[j][i]
+            llk += weights[j][i]
 
-            tot = 0
-            weights[j][i] -= maxi
-            weights[j][i] = np.exp(weights[j][i])
+        tot = 0
+        for j in range (nb_gauss):
+            val = np.exp(weights[j][i] - maxi)
+            weights[j][i] = val
+            tot += val
 
-            tot += weights[j][i]
+        for j in range (nb_gauss):
+            weights[j][i] /= tot
 
-            weights[j][i] = weights[j][i] / tot
+    return llk
 
-            llk += maxi + np.log(tot)
+def compare_mu(a, b):
+    dif = np.empty([len(a), len(a[0])])
 
-    return (weights, llk)
+    for i in range (len(a)):
+        for j in range (len(a[0])):
+            dif[i][j] = np.abs(a[i][j] - b[i][j])
 
-if __name__ == '__main__':
-    nb_pts = 3000
-    nb_gauss = 3
-    dim = 3
+    return dif
 
-    distanceBTWclasses = 10
+def compare_cov(a, b):
+    dif = np.empty([len(a), len(a[0]), len(a[0][0])])
 
-    alpha = np.repeat(1.0 / nb_gauss, nb_gauss)
-    # The mean matrix
-    mu = [(np.random.random(dim) * distanceBTWclasses * i) for i in range(1, nb_gauss + 1)]
+    for i in range (len(a)):
+        for j in range (len(a[0])):
+            for k in range (len(a[0][0])):
+                dif[i][j][k] = np.abs(a[i][j][k] - b[i][j][k])
 
-    cov = get_sdp_mat(4, nb_gauss, dim)
-    data = gen_gmm(alpha, nb_pts, nb_gauss, mu, cov)
+    return dif
 
-    print ("### Output parameters")
+def debug_print(target_mu, target_cov, mu, cov):
+    print ("### Target parameters")
     print ("mu:")
-    print (mu)
+    print (target_mu)
 
     print ("cov:")
-    print (cov)
-
-#    print ("alpha")
-#    print (alpha)
+    print (target_cov)
 
     print ("### Generate parameters")
-
-#   prev = -math.inf
-
-    cov = get_sdp_mat(4, nb_gauss, dim)
-    mu = [(np.random.random(dim) * distanceBTWclasses * i) for i in range(1, nb_gauss + 1)]
-
     print ("Generate mu:")
     print (mu)
     print ("Generate cov:")
     print (cov)
 
-    for i in range (5):
-        (weights, llk) = compute_weight(nb_pts, nb_gauss, dim, alpha, data, mu, cov)
 
-#       prev = llk
+if __name__ == '__main__':
+    nb_pts = 15000
+    nb_gauss = 3
+    dim = 5
 
+    distanceBTWclasses = 30
+
+    alpha = np.repeat(1.0 / nb_gauss, nb_gauss)
+    target_mu = [(np.random.random(dim) * distanceBTWclasses * i) for i in range(1, nb_gauss + 1)]
+    target_cov = get_sdp_mat(4, nb_gauss, dim)
+
+    data = gen_gmm(alpha, nb_pts, nb_gauss, target_mu, target_cov)
+
+    cov = get_sdp_mat(4, nb_gauss, dim)
+    mu = [(np.random.random(dim) * distanceBTWclasses * i) for i in range(1, nb_gauss + 1)]
+
+    #debug_print(target_mu, target_cov, mu, cov)
+    weights = np.empty([nb_gauss, nb_pts])
+
+    prev = -math.inf
+    llk = -9999999 # FIXME
+    i = 0
+
+#   print (compare_mu(target_mu, mu))
+    while i < 10 or llk - prev > 0.01:
+        if i > 30:
+            break
+        """
+        if False and i % 10 == 0:
+            plot_data(data)
+        """
+        i += 1
+        prev = llk
+        llk = compute_weight(weights, nb_gauss, dim, alpha, data, mu, cov)
         for j in range (nb_gauss):
             s_weight = np.sum(weights[j])
 
-            alpha[j] = s_weight / len(data[j])
-            mu[j] = update_mu(s_weight, data[j], weights[j])
-            cov[j] = update_cov(s_weight, data[j], mu[j], weights[j])
-
+            alpha[j] = s_weight / nb_pts
+            update_mu(mu[j], s_weight, data, weights[j])
+            cov[j] = update_cov(s_weight, data, mu[j], weights[j])
 
     print("###After EM:")
-#  print ("s weight")
-#           print (s_weight)
-#    print ("alpha")
-#    print (alpha)
 
-    print ("mu")
-    print (mu)
+    print ("diff mu")
+    print (compare_mu(target_mu, mu))
 
-    print ("cov")
-    print (cov)
+    print ("diff cov")
+    print (compare_cov(target_cov, cov))
+
+    print ("stop after " + str(i) + " iterations")
+    print (prev)
+    print (llk)
